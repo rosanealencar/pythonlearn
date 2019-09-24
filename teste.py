e31effb343b7c31f468fd52f33bde036a24d4cf9
Rosane Alencar <rosane.alencar.s@gmail.com>
2019-09-24 16:52:45 -0400

    Initial testes

---
+++ teste.py
@@ -0,0 +1,9 @@
+#exercicios site: https://wiki.python.org.br/EstruturaSequencial
+
+
+a = int(input('entre com o primeiro valor: '))
+b = int(input('entre com o segundo valor: '))
+result = (a + b)
+print ("A soma é:", result)
+
+
